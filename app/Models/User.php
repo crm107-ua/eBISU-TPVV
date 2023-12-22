cc660ff132e064f774d5dd00ef4b78ba21cc65e0
ea57 <ea57@alu.ua.es>
2023-12-22 12:00:34 +0100

    relacion tecnico-user + test

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(Business::class, 'id');
     }
+
+    public function technician()
+    {
+        return $this->hasOne(Technician::class, 'id');
+    }
 }
