3464cdcaa69affe9d5d925e43f6edb7434ca7578
agp221-ua <agp221@alu.ua.es>
2024-01-23 20:00:11 +0100

    Añadida funcionalidad de denegacion de acceso si no está dado de alta desde rutas que precisen estar dado de alta

--- routes/web.php
+++ routes/web.php
@@ -15,59 +15,59 @@ Route::get('/', function () {
 
 Route::get('/technical-home', function () {
     return view('home.technical-views.technical-home');
-})->middleware(['auth', 'verified', 'technician'])->name('technical-home');
+})->middleware(['auth', 'verified','discharge', 'technician'])->name('technical-home');
 
 Route::get('/business-home', function () {
     return view('home.business-views.business-home');
-})->middleware(['auth', 'verified', 'business'])->name('business-home');
+})->middleware(['auth', 'verified','discharge',  'business'])->name('business-home');
 
 Route::get('/technician/reviews', [TechnicianController::class, 'showTechnicianValorations'])
-    ->middleware(['auth', 'verified', 'technician'])->name('technician.reviews');
+    ->middleware(['auth', 'verified','discharge',  'technician'])->name('technician.reviews');
 
 Route::get('/technician/tickets', [TechnicianController::class, 'showTechnicianTickets'])
-    ->middleware(['auth', 'verified', 'technician'])->name('technician.tickets');
+    ->middleware(['auth', 'verified','discharge',  'technician'])->name('technician.tickets');
 
 Route::post('/technician/tickets/{id}/changeState', [TechnicianController::class, 'changeTicketState'])
-    ->middleware(['auth', 'verified', 'technician'])->name('technician.changeTicketState');
+    ->middleware(['auth', 'verified','discharge',  'technician'])->name('technician.changeTicketState');
 
 
 
 Route::get('/tickets', [TicketController::class, 'showTickets'])
-    ->middleware(['auth', 'verified', 'business'])->name('tickets');
+    ->middleware(['auth', 'verified','discharge',  'business'])->name('tickets');
 
 Route::get('/ticket/{id}', [\App\Http\Controllers\TicketController::class, 'showTicket'])
-    ->middleware(['auth', 'verified', 'ticketAccess'])->name('ticket');
+    ->middleware(['auth', 'verified','discharge',  'ticketAccess'])->name('ticket');
 Route::post('/ticket/{id}/valorate', [\App\Http\Controllers\TicketController::class, 'valorateTicket'])
-    ->middleware(['auth', 'verified', 'ticketAccess'])->name('valorateTicket');
+    ->middleware(['auth', 'verified','discharge',  'ticketAccess'])->name('valorateTicket');
 
 Route::get('/business/token', [BusinessController::class, 'showTokenDetails'])
-    ->middleware(['auth', 'verified', 'business'])->name('business-token');
+    ->middleware(['auth', 'verified','discharge',  'business'])->name('business-token');
 
 Route::get('/business/token/new', [BusinessController::class, 'createNewToken'])
-    ->middleware(['auth', 'verified', 'business'])->name('business-token-new');
+    ->middleware(['auth', 'verified', 'discharge',  'business'])->name('business-token-new');
 
 Route::get('/payments', [BusinessController::class, 'showPayments'])
-    ->middleware(['auth', 'verified', 'business'])->name('payments');
+    ->middleware(['auth', 'verified', 'discharge',  'business'])->name('payments');
 
 Route::get('/payment/{id}', [BusinessController::class, 'showPayment'])
-    ->middleware(['auth', 'verified', 'paymentAccess'])->name('payment');
+    ->middleware(['auth', 'verified', 'discharge',  'paymentAccess'])->name('payment');
 
 Route::get('/payment/{id}/report', [\App\Http\Controllers\TicketController::class, 'showCreateTicket'])
-    ->middleware(['auth', 'verified', 'paymentAccess'])->name('report');
+    ->middleware(['auth', 'verified', 'discharge',  'paymentAccess'])->name('report');
 
 Route::post('/payment/{id}/report', [\App\Http\Controllers\TicketController::class, 'createTicket'])
-    ->middleware(['auth', 'verified', 'paymentAccess'])->name('createReport');
+    ->middleware(['auth', 'verified', 'discharge',  'paymentAccess'])->name('createReport');
 
 Route::get('/terminos-condiciones', function () {
     return view('home.general-views.terminos');
 })->name('terminos-condiciones');
 
 Route::get('/downloadFile/{id}', [TicketController::class, 'downloadFile'])
-    ->middleware(['auth', 'verified', 'attachmentAccess'])->name('downloadFile');
+    ->middleware(['auth', 'verified', 'discharge',  'attachmentAccess'])->name('downloadFile');
 Route::post('/send-email', [EmailController::class, 'sendEmail'])->name('send.email');
 
 Route::post('/ticket/{id}/comment', [\App\Http\Controllers\TicketController::class, 'addComment'])
-    ->middleware(['auth', 'verified', 'ticketAccess'])->name('addComment');
+    ->middleware(['auth', 'verified', 'discharge',  'ticketAccess'])->name('addComment');
 
 Route::get('404', function () {
     $htmlContent = file_get_contents(resource_path('views/dashboard/template/pages/samples/error-404.html'));
